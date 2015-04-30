12862565a3c9ad157af1215a02fe337d783ce2af
Anitha Gollamudi <anitha.gollamudi@gmail.com>
2015-04-30 09:39:14 -0400

    String Tag files

---
+++ klee/build.sh
@@ -0,0 +1,8 @@
+export LD_LIBRARY_PATH=/home/anitha/kleestr/metasmt/deps/boost-1_55_0/lib:/home/anitha/kleestr/metasmt/deps/Z3-4.1/lib
+export PATH=~/kleestr/llvm-gcc4.2-2.9-x86_64-linux/bin/:$PATH
+export C_INCLUDE_PATH=/usr/include/x86_64-linux-gnu
+
+CXXFLAGS="-g -O0" CFLAGS="-g -O0" ./configure --with-llvm=/home/anitha/kleestr/llvm-2.9/ --with-stp=/home/anitha/kleestr/stp-r940/install --with-uclibc=/home/anitha/kleestr/klee-uclibc --enable-posix-runtime --with-metasmt=/home/anitha/kleestr/metasmt/build/root --with-runtime=Debug+Asserts
+make ENABLE_OPTIMIZED=1
+#make check
+#make unittests
---
+++ stp-r940/build.sh
@@ -0,0 +1,2 @@
+./scripts/configure --with-prefix=/home/anitha/kleestr/stp-r940/install --with-cryptominisat2
+make OPTIMIZE=-O2 CFLAGS_M32= install

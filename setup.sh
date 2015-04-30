e8b6e5f2acb9f055c3ad956bc86774ea19a93e0b
Anitha Gollamudi <anitha.gollamudi@gmail.com>
2015-04-30 09:57:23 -0400

    Setup file to be used for running kleestr smoothly

---
+++ setup.sh
@@ -0,0 +1,4 @@
+export PATH=$HOME/kleestr/klee/Release+Asserts/bin:$HOME/kleestr/llvm-2.9/Release+Asserts/bin:$HOME/kleestr/llvm-gcc4.2-2.9-x86_64-linux/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+export LD_LIBRARY_PATH=$HOME/kleestr/metasmt/deps/Z3-git/lib:$HOME/kleestr/metasmt/deps/boost-1_55_0/lib:$HOME/kleestr/metasmt/deps/cvc4-1.4/lib
+export C_INCLUDE_PATH=$HOME/kleestr/klee/include:/usr/include/x86_64-linux-gnu/
+export CPLUS_INCLUDE_PATH=$HOME/kleestr/klee/include:/usr/include/x86_64-linux-gnu/
